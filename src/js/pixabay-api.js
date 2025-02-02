0b390b1c3ca1f6d41195f0b57509a707f1668101
ChNokia <chizarus@gmail.com>
2025-02-02 13:45:07 +0200

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,14 +2,14 @@
 
 import axios from 'axios'
 
-const API_KEY = "47390664-00e9f6e051e23aada86cf3665";
+//const API_KEY = "47390664-00e9f6e051e23aada86cf3665";
 const BASE_URL = "https://pixabay.com/api/";
 export const IMAGES_PER_PAGE = 15;
 
 export async function fetchData(page = 1, searchParams) {
     const { data } = await axios(BASE_URL, {
         params: {
-            key: API_KEY,
+            key: import.meta.env.VITE_API_KEY,
             image_type: "photo",
             orientation: "horizontal",
             safesearch: true,
